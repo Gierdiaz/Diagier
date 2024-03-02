108bb014c17d58396227993e0c417cf946d50a2d
Gierdiaz <gierdiaz@hotmail.com>
2024-03-01 21:09:02 -0300

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -77,6 +77,7 @@ class AuthController extends Controller
                 $secretKey
             );
 
+            //dd();
             return view('auth.2fa', ['QR_Image' => $QR_Image, 'secret' => $secretKey]);
         } catch (\Exception $e) {
             return back()->withError($e->getMessage())->withInput();
