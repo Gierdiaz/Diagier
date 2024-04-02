ed7a51fe7907c324174fe0af60ae4a2baed1519e
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-04-02 14:44:12 -0300

    refactor: remove unused libraries downloaded via CDN
    as we have decided to change the login screen layout, some of the previous
    strategies for styling (i.e., using particlesJs library) are no longer needed.
    In order to make the views cleaner, this commit aims to remove all of these
    libraries and to reuse the sign layout.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Diagier</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-</head>
-<body>
-    <div id="particles-js"></div>
-
-    <div class="container">
+<x-sign-layout>
+    <div>
         <h2>Login</h2>
 
         @if(session('status'))
-            <p>{{ session('status') }}</p>
+        <p>{{ session('status') }}</p>
         @endif
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-    
-            <label for="email">E-mail:</label>
-            <input type="email" name="email" value="{{ old('email') }}" required>
-            @error('email')
+                <label for="email">E-mail</label>
+                <input type="email" name="email" value="{{ old('email') }}" required>
+                @error('email')
                 <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-            
-            <label for="password">Password:</label>
-            <input type="password" name="password" required>
-            @error('password')
+                @enderror
+
+
+                <label for="password">Password</label>
+                <input type="password" name="password" required>
+                @error('email')
                 <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-    
+                @enderror
             <button type="submit">Login</button>
         </form>
 
-        <div class="redirect-links">
+        <div>
             <a href="{{ route('password.request') }}">Forgot password?</a>
-            <span>|</span>
             <a href="{{ route('register') }}">Register</a>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-    <script src="{{ asset('js/particles.js') }}"></script>
-</body>
-</html>
+</x-sign-layout>
